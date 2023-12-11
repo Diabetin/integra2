232a2ae70d31b383e1cb5f1a2a016e0a26757fdc
Diabetin <alexander.acosta@inacapmail.cl>
2023-12-11 01:54:27 -0300

    Segunda carga

--- app/src/main/java/com/example/gpstest/MainActivity.kt
+++ app/src/main/java/com/example/gpstest/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.gpstest
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.location.LocationListenerCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,19 +25,38 @@ class MainActivity : AppCompatActivity() {
         var btn_gps = findViewById<Button>(R.id.btn_gps);
         var txtGPS = findViewById<TextView>(R.id.txtGPS);
 
-        btn_gps.setOnClickListener(new View.OnClickListener())
+        btn_gps.setOnClickListener(object : OnClickListener {
+            override fun onClick(v: View) {
+                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager;
+
+                val locationListener = object : LocationListenerCompat{
+                    override fun onLocationChanged(location: Location) {
+
+                    }
+                    public fun onStatusChanged(provider:String,status:Int, extras:Bundle){}
 
 
-        var permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+                    public override fun onProviderEnabled(provider:String){}
+
+
+                    public override fun onProviderDisabled(provider:String){}
+                };
+                val activity = this as Activity;
+                var permissionCheck = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener)
+
+            }
+        })
+
+        var permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
         if(permissionCheck == PackageManager.PERMISSION_DENIED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
 
             }else{
-                ActivityCompat.requestPermissions(this, new String {android.Manifest.permission.ACCESS_FINE_LOCATION},1);
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+
             }
         }
-
-
     }
 }
